1070f93dee017e69b379932203ba5a470bd93637
MintGhost <sadgabriel@protonmail.com>
2025-08-07 23:40:31 +0900

    add credits

--- Assets/Scripts/TitleUi.cs
+++ Assets/Scripts/TitleUi.cs
@@ -3,21 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class TitleUi : MonoBehaviour
 {
-    [SerializeField] private GameObject creditPanel;
 
     public void OnClickStartGame()
     {
         SceneManager.LoadScene("GameScene");
-    }
-
-    public void OnClickCredit()
-    {
-        creditPanel.SetActive(true);
-    }
-
-    public void OnClickBackFromCredit()
-    {
-        creditPanel.SetActive(false);
     }
 
     public void OnClickExit()
