298f6131cf41c0ca8788e925fc844a07737400aa
MintGhost <sadgabriel@protonmail.com>
2025-08-12 19:38:43 +0900

    modify IdlePanel UI

--- Assets/Scripts/UI/IdlePanel.cs
+++ Assets/Scripts/UI/IdlePanel.cs
@@ -7,6 +7,20 @@ public class IdlePanel : Panel<NoData>
     [SerializeField] private TextMeshProUGUI hpText;
     [SerializeField] private TextMeshProUGUI staminaText;
     [SerializeField] private RawImage selectedSpecialActionIcon;
+    [SerializeField] private Image HpBar;
+    [SerializeField] private Image StaminaBar;
+
+    private float BarMaxWidth;
+    private float BarLeftEdge;
+
+    protected void Awake()
+    {
+        if (HpBar != null)
+        {
+            BarMaxWidth = HpBar.rectTransform.sizeDelta.x;
+            BarLeftEdge = HpBar.rectTransform.anchoredPosition.x - (BarMaxWidth * 0.5f);
+        }
+    }
 
     private void Update()
     {
@@ -15,12 +29,28 @@ public class IdlePanel : Panel<NoData>
 
         if (hpText != null)
         {
-            hpText.text = $"HP: {player.Hp}";
+            hpText.text = $"HP: {player.Hp}/{player.MaxHp}";
+        }
+
+        if (HpBar != null)
+        {
+            float hpPercentage = (float)player.Hp / player.MaxHp;
+            HpBar.rectTransform.sizeDelta = new Vector2(BarMaxWidth * hpPercentage, HpBar.rectTransform.sizeDelta.y);
+
+            HpBar.rectTransform.anchoredPosition = new Vector2(BarLeftEdge + 0.5f * BarMaxWidth * hpPercentage, HpBar.rectTransform.anchoredPosition.y);
         }
 
         if (staminaText != null)
         {
-            staminaText.text = $"Stamina: {player.Stamina}";
+            staminaText.text = $"Stamina: {player.Stamina}/{player.MaxStamina}";
+        }
+
+        if (StaminaBar != null)
+        {
+            float staminaPercentage = (float)player.Stamina / player.MaxStamina;
+            StaminaBar.rectTransform.sizeDelta = new Vector2(BarMaxWidth * staminaPercentage, StaminaBar.rectTransform.sizeDelta.y);
+
+            StaminaBar.rectTransform.anchoredPosition = new Vector2(BarLeftEdge + 0.5f * BarMaxWidth * staminaPercentage, StaminaBar.rectTransform.anchoredPosition.y);
         }
 
         if (selectedSpecialActionIcon != null && SpecialActionManager.Instance.SelectedSpecialAction != null)
